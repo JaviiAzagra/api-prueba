0c73be212fdb2b7666dd772878144ef761f3fa60
JaviiAzagra <javiazagra12@gmail.com>
2024-10-05 01:14:46 +0200

    Refactor peripherals model and routes

--- src/api/models/peripherals.model.js
+++ src/api/models/peripherals.model.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const peripheralsSchema = new Schema(
-    {
-        brand: { type: String, required: true},
-        type: { type: String, required: true },
-        price: { type: Number, required: true},
-        img: { type: String, required: true }
-    },
+  {
+    title: { type: String, required: true },
+    price: { type: String, required: true },
+    img: { type: String, required: true },
+    type: { type: String, required: true },
+  },
 
-    {
-        timestamps: true
-    }
+  {
+    timestamps: true,
+  }
 );
 
 const Peripherals = mongoose.model("peripherals", peripheralsSchema);
--- src/api/routes/peripherals.routes.js
+++ src/api/routes/peripherals.routes.js
@@ -4,34 +4,34 @@ const Peripheral = require("../models/peripherals.model");
 const { uploadFile, deleteFile } = require("../middlewares/cloudinary");
 
 router.get("/", async (req, res, next) => {
-    try {
-        const allPeripherals = await Peripheral.find();
-        res.status(200).json(allPeripherals);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const allPeripherals = await Peripheral.find();
+    res.status(200).json(allPeripherals);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.get("/:id", async (req, res, next) => {
-    try {
-        const onePeripheral = await Peripheral.findById(req.params.id);
-        res.status(200).json(onePeripheral);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  try {
+    const onePeripheral = await Peripheral.findById(req.params.id);
+    res.status(200).json(onePeripheral);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.get("/brand/:brand", async (req, res, next) => {
-    try {
-        const brand = req.params.brand;
-        const peripheralToFind = await Peripheral.find({ brand: brand });
-        return res.status(200).json(peripheralToFind);
-    } catch (error) {
-        return next(error);
-    }
+  try {
+    const brand = req.params.brand;
+    const peripheralToFind = await Peripheral.find({ brand: brand });
+    return res.status(200).json(peripheralToFind);
+  } catch (error) {
+    return next(error);
+  }
 });
 
-router.post("/create", uploadFile.single("img"), async (req, res, next) => {
+/* router.post("/create", uploadFile.single("img"), async (req, res, next) => {
     try {
         const peripheral = req.body;
         if (req.file) {
@@ -43,36 +43,71 @@ router.post("/create", uploadFile.single("img"), async (req, res, next) => {
     } catch (error) {
         return next(error);
     }
+}); */
+
+router.post("/create", uploadFile.array("img", 10), async (req, res, next) => {
+  // Cambia el "10" por el número máximo de imágenes que esperas
+  try {
+    let peripherals = req.body;
+
+    // Si req.body es un solo objeto, conviértelo en un array para manejar ambos casos (uno o múltiples objetos)
+    if (!Array.isArray(peripherals)) {
+      peripherals = [peripherals];
+    }
+
+    // Agregar imágenes subidas a cada peripheral
+    if (req.files) {
+      peripherals.forEach((peripheral, index) => {
+        if (req.files[index]) {
+          peripheral.img = req.files[index].path;
+        }
+      });
+    }
+
+    // Insertar varios "peripherals" en MongoDB
+    const createdPeripherals = await Peripheral.insertMany(peripherals);
+
+    // Responder con los documentos creados
+    return res.status(201).json(createdPeripherals);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 router.put("/edit/:id", uploadFile.single("img"), async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const updatedData = { ...req.body };
+  try {
+    const { id } = req.params;
+    const updatedData = { ...req.body };
 
-        if (req.file) {
-            updatedData.image = req.file.path;
-        }
-
-        const updatedPeripheral = await Peripheral.findByIdAndUpdate(id, updatedData, { new: true });
-        return res.status(200).json(updatedPeripheral);
-    } catch (error) {
-        return next(error);
+    if (req.file) {
+      updatedData.image = req.file.path;
     }
+
+    const updatedPeripheral = await Peripheral.findByIdAndUpdate(
+      id,
+      updatedData,
+      { new: true }
+    );
+    return res.status(200).json(updatedPeripheral);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 router.delete("/delete/:id", async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const peripheral = await Peripheral.findById(id);
-        if (peripheral.img) {
-            deleteFile(peripheral.img);
-        }
-        const peripheralToDelete = await Peripheral.findByIdAndDelete(id);
-        return res.status(200).json(`The peripheral has been deleted --> ${peripheralToDelete}`);
-    } catch (error) {
-        return next(error);
+  try {
+    const { id } = req.params;
+    const peripheral = await Peripheral.findById(id);
+    if (peripheral.img) {
+      deleteFile(peripheral.img);
     }
+    const peripheralToDelete = await Peripheral.findByIdAndDelete(id);
+    return res
+      .status(200)
+      .json(`The peripheral has been deleted --> ${peripheralToDelete}`);
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = router;
